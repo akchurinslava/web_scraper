eb8b4c8f9ae1ed9302dba7b3aacbc3532f5a0a60
Vyacheslav Akchurin <118188091+akchurinslava@users.noreply.github.com>
2024-04-16 23:17:43 +0300

    Fourth commit
    1. Modified requirements.txt. We try to follow latest version of libraries.
    2. Parser except some Exception is finished.
    3. Realized mapping. All car's data is storing inside dict, dict is an objects of list.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -16,18 +17,33 @@ class Auto24Parser:
         """
         load_dotenv()
         self.service = Service(
-            executable_path=os.getenv("EXECUTABLE_PATH"),
-            service_log_path=os.devnull
+            executable_path=os.getenv("EXECUTABLE_PATH"), service_log_path=os.devnull
         )
         self.options = webdriver.ChromeOptions()
         self.options.binary_location = os.getenv("BINARY_LOCATION")
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(service=self.service, options=self.options)
-        self.cars = []
+        self.cars_links = []
         self.input_car_make = input_car_make.lower()
         self.input_page_amout = input_page_amount
         self.cars_list = []
+        self.mapping = {
+            'field-liik': 'type',
+            'field-keretyyp': 'body_type',
+            'field-month_and_year': 'reg',
+            'field-mootorvoimsus': 'engine',
+            'field-kytus': 'fuel',
+            'field-labisoit': 'mileage',
+            'field-vedavsild': 'drive',
+            'field-kaigukast_kaikudega': 'transmission',
+            'field-varvus': 'color',
+            'field-reg_nr': 'reg_number',
+            'field-tehasetahis': 'vin',
+            'field-hind': 'price',
+            'field-soodushind': 'bargain_price',
+            'field-eksporthind': 'export_price',
+        }
 
     def parse(self) -> list:
         """
@@ -37,23 +53,56 @@ class Auto24Parser:
         As you can see finally we store it in list.
         """
         page_count = 0
-        if self.input_car_make == 'all':
-            base_url = f'https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a={types.get('all_types')}'
+        if self.input_car_make == "all":
+            base_url = (
+                f'https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a={types.get('all_types')}'
+            )
         while True:
             base_url = f'https://eng.auto24.ee/kasutatud/nimekiri.php?bn=2&a={types.get('all_types')}&b={makes.get(self.input_car_make)}&af=100&ak={page_count}'
             self.driver.get(base_url)
             soup = BeautifulSoup(self.driver.page_source, "lxml")
             div = soup.find("div", id="usedVehiclesSearchResult-flex")
             if div:
-                listings = div.find_all("a", class_='row-link', href=True)
+                listings = div.find_all("a", class_="row-link", href=True)
                 for listing in listings:
                     car_url = f'https://eng.auto24.ee{listing["href"]}'
-                    self.cars.append(car_url)
-                for car in (self.cars):
-                    self.driver.get(car)
-                    self.cars_list.append(car)
+                    # car_url = 'https://eng.auto24.ee/soidukid/4019764'
+                    self.cars_links.append(car_url)
+                    self.driver.get(car_url)
+                    soup_car = BeautifulSoup(self.driver.page_source, "lxml")
+                    table = soup_car.find("table", class_="main-data")
+                    self.car_info = {}
+                    self.driver.get(car_url)
+                    h1 = soup_car.find("h1", class_="commonSubtitle").text.split()
+                    self.car_info['make'], self.car_info['model'] = h1[0], h1[1]
+                    for tr in table.find_all("tr"):
+                        td_field = tr.find("td", class_="field")
+                        span_value = td_field.find("span", class_="value")
+                        tr_class = tr.attrs.get("class")[0]
+                        if span_value == None:
+                            self.car_info[self.mapping[tr_class]] = ''
+                        else:
+                            if tr_class in ['field-labisoit']:
+                                clean_mileage = span_value.text
+                                clean_mileage = re.findall(r'\d+', clean_mileage)
+                                clean_mileage = int(''.join(clean_mileage))
+                                self.car_info[self.mapping[tr_class]] = clean_mileage
+                            elif tr_class in ['field-hind', 'field-soodushind', 'field-eksporthind']:
+                                clean_price = span_value.text
+                                clean_price = int(re.sub(r'EUR\xa0|,', '', clean_price))
+                                self.car_info[self.mapping[tr_class]] = clean_price
+                                if tr_class in ['field-hind'] and td_field.find("span", class_="vat-value"):
+                                    vat_value = td_field.find("span", class_="vat-value").text
+                                    vat_value = re.findall(r'\d+', vat_value)
+                                    vat_value = int(''.join(vat_value))
+                            else:
+                                self.car_info[self.mapping[tr_class]] = span_value.text
+                    self.car_info['vat'] = vat_value
+                    self.car_info['link'] = car_url
+                    self.cars_list.append(self.car_info)
             else:
                 break
+            page_count += 100
             try:
                 next_page = self.driver.find_element(By.CSS_SELECTOR, "button.btn.btn-right")
                 if next_page.get_attribute("class") == "btn btn-right disabled":
@@ -61,25 +110,25 @@ class Auto24Parser:
             except:
                 if input_page_amount == page_count:
                     break
-            page_count += 100
-            print(self.cars_list)
 
     def get_cars(self):
-        return self.cars
+        return self.cars_list
 
     def close(self):
         self.driver.quit()
 
 
 if __name__ == "__main__":
-    input_car_make = input('Введите марку машины: ')
-    input_page_amount = input('Сколько страниц сканировать: ')
+    input_car_make = input("Введите марку машины: ")
+    input_page_amount = input("Сколько страниц сканировать: ")
     try:
         input_page_amount = int(input_page_amount) * 100
     except:
         input_page_amount = input_page_amount
     parser = Auto24Parser(input_car_make, input_page_amount)
     parser.parse()
-    car_links = parser.get_cars()
-    # print(car_links)
+    for i in parser.get_cars():
+        print(i)
+        print()
+    print(len(parser.get_cars()))
     parser.close()
